9b8c80daf4259b28f03c2a69e9ffe6b56fd680ad
kamidem <77845156+kamidem@users.noreply.github.com>
2021-04-20 11:33:09 +0300

    created base App body, started adding Dictionary element

---
+++ src/js/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Dictionary from './Dictionary.js'
+import '../css/App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header>
+        the dictionary
+      </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer>
+        Coded by Kamile Dementaviciute
+      </footer>
+    </div>
+  );
+}
+
+
