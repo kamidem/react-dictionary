f680d747b3f3856bb5b932efb85e35b8d89e4848
kamidem <77845156+kamidem@users.noreply.github.com>
2021-04-20 12:16:53 +0300

    added basic search form (not functioning yet)

--- src/js/Dictionary.js
+++ src/js/Dictionary.js
@@ -1,5 +1,29 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
+
 
 export default function Dictionary() {
-  return <div>Hello from dictionary</div>
+  let [word, setWord] = useState('');
+
+  function handleResponse() {
+
+  }
+
+  function search(event) {
+    event.preventDefault();
+    alert(`searching for ${word} definition`);
+
+    let apiUrl = 'https://api.dictionaryapi.dev/api/v2/entries/en_US/hello';
+    axios.get(apiUrl).then(handleResponse)
+  }
+
+  function handleWordChange(event) {
+    setWord(event.target.value);
+  }
+
+  return <div className='search-block' >
+    <form onSubmit={search} >
+      <input type='search' placeholder='search a word' onChange={handleWordChange} />
+    </form>
+  </div>
 }
